652a214f1cf2403c9dff6f5d8930a221baa3d722
Sam Wu <Wu@Sams-MacBook-Air.local>
2016-07-21 08:29:18 -0400

    Adds ItemQueueViewController

---
+++ MonitorDemo/ItemQueueViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ItemQueueViewController.swift
+//  MonitorDemo
+//
+//  Created by Sam Wu on 7/21/16.
+//  Copyright © 2016 BothPoints. All rights reserved.
+//
+
+import UIKit
+
+class ItemQueueViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
